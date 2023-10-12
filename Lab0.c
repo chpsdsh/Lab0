ac2ed36977afe44b9dc2195e62c3da547c53781b
chpsdsh <bmxsib1@gmail.com>
2023-10-12 12:16:59 +0700

    master

--- Lab0.c
+++ Lab0.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
+#include <ctype.h>
+
+void proverkab1 (char *chislo, int b1){
+    int cnt = 0;
+        for(int i = 0; i < strlen(chislo); i++)
+            if(((int)chislo[i] > (int)'0' && (int)chislo[i] <= (int)((b1 - 1) + '0')) || chislo[i] == '.' || (((int)chislo[i] - (int)'0' - b1 - 7 < 0)))
+                cnt++;
+
+    if (cnt != strlen(chislo))
+        printf("bad input");
+}
 
 int main(){
+    int b1, b2;
+    char chislo[13];
+    scanf("%d %d\n",&b1, &b2);
+    scanf("%s",&chislo);
+    if(b1 < 2 || b1 > 16 || b2 < 2 || b2 > 16){
+        printf("bad input");
+        return 0;
+    }
+    proverkab1(chislo,b1);
+
+
     return 0;
+
 }
